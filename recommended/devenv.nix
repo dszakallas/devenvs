41f53e09979a1ca9801462c20a8ffb75b7c3232f
Dávid Szakállas <david.szakallas@gmail.com>
2025-02-15 17:08:02 +0100

    add recommended

---
+++ recommended/devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  # Recommended to always add git, the system version can be outdated.
+  # It also resolves a color highlighting issue in the terminal for me.
+  packages = with pkgs; [ git ];
+
+  # Always enable nix language support.
+  language.nix.enable = true;
+
+  pre-commit.hooks = {
+    nixfmt-rfc-style.enable = true;
+    markdownlint = {
+      enable = true;
+      settings.configuration = {
+        MD013.line_length = 120;
+      };
+    };
+    shellcheck.enable = true;
+  };
+}
